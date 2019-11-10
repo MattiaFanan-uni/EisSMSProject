555e47de7375403f43806b9ab8ded1b41b3a4b01
MattiaFanan-uni <mattia.fanan@studenti.unipd.it>
2019-11-10 14:21:06 +0100

    added standard android studio project

---
+++ app/src/main/java/com/example/eissmsproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.eissmsproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
