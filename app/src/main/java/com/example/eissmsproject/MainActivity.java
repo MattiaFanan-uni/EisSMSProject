df919d2a2c158bb26dc931675a0b295e85c10797
Riccardo Crociani <riccardo.crociani@studenti.unipd.it>
2019-11-10 16:07:22 +0100

    Added layout

--- app/src/main/java/com/example/eissmsproject/MainActivity.java
+++ app/src/main/java/com/example/eissmsproject/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.eissmsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendButton(View view) {
+
     }
 }
